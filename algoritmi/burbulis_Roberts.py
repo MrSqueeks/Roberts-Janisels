bfb6e77f8d4f23875a2e2f44fc4bd8d971240b21
MrSqueeks <144416764+MrSqueeks@users.noreply.github.com>
2024-01-16 13:52:07 +0200

    Update burbulis_Roberts.py

--- algoritmi/burbulis_Roberts.py
+++ algoritmi/burbulis_Roberts.py
@@ -1,11 +1,3 @@
-def burbulis(saraksts):
-    for key,value in saraksts.items():
-        if(saraksts[key]<saraksts[key+1]):
-            temp=saraksts[key]
-            saraksts[key]=saraksts[key+1]
-            saraksts[key+1]=temp
-   
-
 def sakartot():
     elementi = int(input("Ievadiet studentu skaitu sarakstā: "))
     saraksts = {}
@@ -13,7 +5,7 @@ def sakartot():
     for i in range(elementi):
         vards = input(f"Ievadiet {i+1}. skolēna vārdu: ")
         atzime = int(input(f"Ievadiet {i+1}. atzīmi: "))
-    saraksts[vards] = atzime
+        saraksts[vards] = atzime
     marklist=list(saraksts.items())
     l=len(saraksts)
     for i in range(l-1):
@@ -23,6 +15,13 @@ def sakartot():
                 marklist[i]=marklist[j]
                 marklist[j]=t
     sortdict=dict(marklist)
-    print(sortdict)
-
+    uz_augsu_vai_leju = input("Kā jūs vēlaties printēt sarakstu? (up/down)")
+    if uz_augsu_vai_leju == 'up':
+        print("\n".join("{}\t{}".format(k, v) for k, v in sortdict.items()))
+    elif uz_augsu_vai_leju == 'down':
+        uz_leju_sakaroti_dati = sorted(saraksts.items(), key=lambda x:x[1], reverse=True)
+        converted_dict = dict(uz_leju_sakaroti_dati)
+        print(converted_dict)
+    else:
+        print('Nepareizi ievadīti dati')
 sakartot()
